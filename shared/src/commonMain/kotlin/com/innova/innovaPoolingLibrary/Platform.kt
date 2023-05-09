97f5facde9260a5c3217f3513d19d933ad2275f2
yukti.vyas <yukti.vyas@acsicorp.com>
2023-05-09 14:57:10 +0530

    Phone number and email validation

--- shared/src/commonMain/kotlin/com/innova/innovaPoolingLibrary/Platform.kt
+++ shared/src/commonMain/kotlin/com/innova/innovaPoolingLibrary/Platform.kt
@@ -5,3 +5,10 @@ interface Platform {
 }
 
 expect fun getPlatform(): Platform
+
+class PhoneNumberEmailValidator() {
+    private val mobilePattern = "[0-9]{10}"
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+    fun isPhoneNumberValid(phoneNumber: String) = phoneNumber.matches(Regex(mobilePattern))
+    fun isEmailValid(email: String) = email.matches(Regex(emailPattern))
+}
