f82939b6ea8e6a7e62c1f9bc4105ff57ecc46c38
Kundeti Sai Pratyusha <78682975+Pratyusha114@users.noreply.github.com>
2023-05-12 12:50:59 +0530

    Regex utils (#3)
    * Created res folder for strings, colors, fonts, dimensions (#1)
    
    * Created res folder for strings, colors, fonts, dimensions
    
    * Updated changes after review
    
    ---------
    
    Co-authored-by: yukti.vyas <yukti.vyas@acsicorp.com>
    
    * Phone number and email validation (#2)
    
    Co-authored-by: yukti.vyas <yukti.vyas@acsicorp.com>
    
    * Created a RegexUtilsClass to handle phone number, Email, and number plate.
    
    * Regex utils file name changed
    
    ---------
    
    Co-authored-by: Yukti Vyas <132065924+vyukti13@users.noreply.github.com>
    Co-authored-by: yukti.vyas <yukti.vyas@acsicorp.com>
    Co-authored-by: pratyusha.kundei <pratyusha.kundei@acsicorp.com>

---
+++ shared/src/commonMain/kotlin/com/innova/innovaPoolingLibrary/res/values/Dimensions.kt
@@ -0,0 +1,32 @@
+package com.innova.innovaPoolingLibrary.res.values
+
+object Paddings {
+    const val padding1 = 1
+    const val padding2 = 2
+    const val padding3 = 3
+    const val padding4 = 4
+    const val padding5 = 5
+    const val padding6 = 6
+    const val padding7 = 7
+    const val padding8 = 8
+    const val padding9 = 9
+    const val padding10 = 10
+
+    const val padding12 = 12
+    const val padding14 = 14
+    const val padding16 = 16
+    const val padding18 = 18
+    const val padding20 = 20
+    const val padding30 = 30
+
+}
+
+object CornerRadius {
+    const val cornerRadius1 = 1
+    const val cornerRadius2 = 2
+    const val cornerRadius3 = 3
+    const val cornerRadius4 = 4
+    const val cornerRadius5 = 5
+    const val cornerRadius10 = 10
+
+}
---
+++ shared/src/commonMain/kotlin/com/innova/innovaPoolingLibrary/utils/regexUtils/RegexUtils.kt
@@ -0,0 +1,19 @@
+package com.innova.innovaPoolingLibrary.utils.regexUtils
+
+class RegexUtils {
+
+
+    private val regexStateRegistration = "^[A-Z]{2}[\\\\ -]?\\d{2}[\\\\ -]?[A-Z]{1,2}[\\\\ -]?\\d{4}\$"
+    private val regexBharatRegistration = "^\\d{2}[\\\\ -]?BH{1,2}[\\\\ -]?\\d{4}[\\\\ -]?[A-Z]{2}\$"
+    private val mobilePattern = "[0-9]{10}"
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+    fun isPhoneNumberValid(phoneNumber: String) = phoneNumber.matches(Regex(mobilePattern))
+
+    fun isEmailValid(email: String) = email.matches(Regex(emailPattern))
+
+    fun isVehicleNumberValid(vehicleNumber: String) : Boolean{
+        return vehicleNumber.matches(Regex(regexStateRegistration)) ||
+                vehicleNumber.matches(Regex(regexBharatRegistration))
+    }
+}
